c6ea228d25c7d2b12d98c5945d9cf61a3be65a55
Spoorti143 <spoortichavan6@gmail.com>
2024-08-21 21:31:23 +0530

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all js code  as newer version
+
+// alert(3+3)   //we are using node.js not chrome
+
+console.log(3+
+    3) // code reliability should be high
+ console.log("spurthi")
+
+let name="spoo"
+let age=20
+let isLoggedIn=false
+
+console.log()
+
+// number =>2 to power of 53
+// bigint
+// string=>""
+//boolean =>true/false
+// null =>standalone value :emptyy
+//undefined =>it means undeclared value
+//symbol =>unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof "spurti") //string
+console.log(typeof 23) //number
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId=1234
+let accountEmail="sporthi@gmail.com"
+var accountPssword="1234"
+var accountCity = "jaipur"
+let accountState;
+
+//accountId=2// not allowed
+
+
+accountEmail="spoo@gmail.com"
+accountPassword="12332"
+accountCity="bengaluru"
+
+
+console.log("accountId");
+console.table([accountId,accountEmail, accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+becoz it creates issue in block scope and functional scope
+*/
+
+
+console.log("ashu")
