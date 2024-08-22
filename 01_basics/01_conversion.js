e32ff4a2e23ab45e253b015d8675b7ac4d91a20a
Spoorti143 <spoortichavan6@gmail.com>
2024-08-22 20:27:47 +0530

    datatypes

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,31 @@
+let score="spurthi"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //output NaN=not a number
+
+//"33" => easily converts to 33
+//"abc" => it dos'nt convert to NaN
+//true =>it converts to 1
+//false => it converts to 0
+
+let isLoggedIn="spoorthi"
+
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// if we give 1 then its true 
+//if we give 0 then its false
+//"" it gives false
+//"spurthi" it gives true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log( stringNumber);
+console.log(typeof stringNumber);
+
