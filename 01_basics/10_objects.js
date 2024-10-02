87d913d6d16967a2726ba6c5b05c16cbc6e4cf42
Spoorti143 <spoortichavan6@gmail.com>
2024-10-02 21:41:16 +0530

    10_objects

--- 01_basics/10_objects.js
+++ 01_basics/10_objects.js
@@ -7,10 +7,13 @@
 //Object.create
 
 //declaration of symbol
+const mySymb=Symbol("mykey1")//interview question print symbol key
+
 //declaration of object
 const jsuser={
     name:"spoorthi",
     "full name":["spoorthi","chavan"],
+    mySymb:"key1",//act as key
     age:21,
     location:"belgaum",
     email:"spoorthichavan143@gmail.com",
@@ -19,5 +22,21 @@ const jsuser={
 }
 console.log(jsuser.email);
 console.log(jsuser["email"]);
-console.log(jsuser.");
+console.log(jsuser["full name"]);
+console.log(jsuser[mySymb]);
 
+jsuser.email="spoorthichavan143@gmail.com"
+//Object.freeze(jsuser)
+jsuser.email="spoorthichatgpt.gmail.com"
+console.log(jsuser);
+
+//function declaration
+jsuser.greeting=function(){
+    console.log("my name is spoo");
+    }
+jsuser.greetingTwo=function(){
+    console.log('my name is spoo ','${this.name}')
+    }
+console.log(jsuser.greeting());
+console.log(jsuser.greetingTwo());
+    
